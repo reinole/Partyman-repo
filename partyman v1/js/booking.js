0cebbaa491b28e5775fed2e2234f36599dd8a908
Hans Petter Naumann <Hans Petter Naumann>
2016-11-10 20:36:34 +0000

    validation framework and such

--- partyman v1/js/booking.js
+++ partyman v1/js/booking.js
@@ -4,8 +4,47 @@ function openNext(id){
 	element.setAttribute("open", "");
 	element.scrollIntoView();
 }
+
+/*
+function checkValidity(id){
+    console.log(id);
+    //console.log(e);
+    var form = $( "id" + "-form" );
+    form.validate();
+    if(!form.valid()){
+        e.stopPropagation(); 
+    }
+}
+*/
+    
+$('.step-continue').click(function (e) {
+    var id = e.target.id;
+    var formid = (id + "-form");
+    var form = $('#' + formid);
+    var icon = $('#' + id + '-valid-icon');
+    form.validate();
+    if(!form.valid()){
+        console.log('form not valid');
+        e.stopPropagation(); 
+        icon.hide();
+    }
+    else{
+        console.log('form valid');
+        icon.show();
+        
+        var step = parseInt(formid.charAt(5)) + 1;
+        var next = "container-step-" + step;
+        var element = $('#' + next);
+       // element[0].scrollToTop();
+        element[0].scrollIntoView();
+        
+    }
+
+   // e.stopPropagation();   
+    // some action ...
+});
 //slideshow
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -30,7 +69,7 @@ function showSlides(n) {
     } 
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-}
+}*/
 
 $(function () {
     $('#datetimepicker12').datetimepicker({
